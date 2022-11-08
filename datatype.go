de6c0d72dc21160a598e3b70e836d9a6198c5eed
Lu-Qiao <luqiao@andrew.cmu.edu>
2022-11-08 12:41:02 -0500

    Update datatype.go

--- datatype.go
+++ datatype.go
@@ -1,2 +1,24 @@
 package main
-cell
+
+// cell
+// state string
+// conc of virus float
+// position orderedPair
+type cell struct {
+	state     string
+	concVirus float64
+	position  orderedPair
+}
+
+// tissue
+// width
+type tissue struct {
+	width int
+}
+
+type orderedPair struct {
+	x, y int
+}
+
+// orderedPair
+// int
