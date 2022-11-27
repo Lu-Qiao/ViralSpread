432ad438adc4e39a8546797ee1c1eb601bb1ee4f
luotao9728 <112722913+luotao9728@users.noreply.github.com>
2022-11-26 23:17:50 -0500

    Update functions_tao.go

--- functions_tao.go
+++ functions_tao.go
@@ -4,7 +4,7 @@ package main
 // with initialBoard using a time step of timeStep seconds.
 // Input: a Board object initialBoard, a int of generations parameter numGens, and a float64 time interval timeStep.
 // Output: a slice of numGens + 1 total Board objects.
-func SimulateViralSpread(initialBoard Board, numGens int, timeteps float64) []Board {
+func SimulateViralSpread(initialBoard Board, numGens int, timeSteps float64) []Board {
 	timePoints := make([]Board, numGens+1)
 	timePoints[0] = initialBoard
 
@@ -15,4 +15,39 @@ func SimulateViralSpread(initialBoard Board, numGens int, timeteps float64) []Bo
 
 	return timePoints
 
+}
+
+// UpdateBoard
+// Input:
+// Output:
+func UpdateBoard(currentBoard Board, timeSteps float64) Board {
+	newBoard := CopyBoard(currentBoard)
+
+	for i := range newBoard {
+		for j := range newBoard[i] {
+			_ = j
+		}
+
+	}
+
+	return newBoard
+}
+
+// CopyBoard
+// Input:
+// Output:
+func CopyBoard(currentBoard Board) Board {
+	// initialize an empty board according to the input board
+	newBoard := make([][]Cell, len(currentBoard))
+	for i := range currentBoard {
+		newBoard[i] = make([]Cell, len(currentBoard[0]))
+	}
+	// deep copy all values in the board
+	for i := range newBoard {
+		for j := range newBoard[0] {
+			newBoard[i][j].state = currentBoard[i][j].state
+			newBoard[i][j].concVirus = currentBoard[i][j].concVirus
+		}
+	}
+	return newBoard
 }
