3555e7c6b906e4c70d4fa8f251b9868c951a4512
luotao9728 <112722913+luotao9728@users.noreply.github.com>
2022-11-26 22:40:47 -0500

    Update datatype.go

--- datatype.go
+++ datatype.go
@@ -3,24 +3,12 @@ package main
 // cell
 // state string
 // conc of virus float
-// position orderedPair
 type Cell struct {
 	// states: "Infectious", "Infected",
 	// "dead", and "Uninfected"
 	state     string
 	concVirus float64
-	position  orderedPair
 }
 
-// tissue
-type Tissue [][]Cell
-
-type orderedPair struct {
-	x, y int
-}
-
-// Use Board to set the position of cells
+// Board
 type Board [][]*Cell
-
-// orderedPair
-// int
