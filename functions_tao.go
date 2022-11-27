98f3421f5b36bb9476b7d1fc6086c5189468faf6
luotao9728 <112722913+luotao9728@users.noreply.github.com>
2022-11-26 23:03:20 -0500

    Update functions_tao.go

--- functions_tao.go
+++ functions_tao.go
@@ -1,1 +1,18 @@
 package main
+
+// SimulateViralSpread simulates the viral spread system over numGens generations starting
+// with initialBoard using a time step of timeStep seconds.
+// Input: a Board object initialBoard, a int of generations parameter numGens, and a float64 time interval timeStep.
+// Output: a slice of numGens + 1 total Board objects.
+func SimulateViralSpread(initialBoard Board, numGens int, timeteps float64) []Board {
+	timePoints := make([]Board, numGens+1)
+	timePoints[0] = initialBoard
+
+	// now range over the number of generations and update the Board each time
+	for i := 1; i <= numGens; i++ {
+		timePoints[i] = UpdateBoard(timePoints[i-1], timeSteps)
+	}
+
+	return timePoints
+
+}
