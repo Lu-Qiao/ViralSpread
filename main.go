00b4a814016cb74a8db08a2c86d09e3279835244
luotao9728 <luotao9728@gmail.com>
2022-11-27 19:54:34 -0500

    Update main.go

--- main.go
+++ main.go
@@ -1,4 +1,52 @@
 package main
 
+import (
+	"os"
+	"strconv"
+)
+
 func main() {
+	// os.Args[0] is program name ("ViralSpread")
+
+	// os.Args[1] takes numInfectious argument
+	numInfectious, err1 := strconv.Atoi(os.Args[1])
+	if err1 != nil {
+		panic(err1)
+	}
+	_ = numInfectious
+
+	// os.Args[2] takes numGens argument
+	numGens, err2 := strconv.Atoi(os.Args[2])
+	if err2 != nil {
+		panic(err2)
+	}
+	_ = numGens
+
+	// os.Args[3] takes timeSteps argument
+	timeSteps, err3 := strconv.ParseFloat(os.Args[3], 64)
+	if err3 != nil {
+		panic(err3)
+	}
+	_ = timeSteps
+
+	// os.Args[4] takes lambda argument
+	lambda, err4 := strconv.ParseFloat(os.Args[4], 64)
+	if err4 != nil {
+		panic(err4)
+	}
+	_ = lambda
+
+	// os.Args[5] takes omega argument
+	omega, err5 := strconv.ParseFloat(os.Args[5], 64)
+	if err5 != nil {
+		panic(err5)
+	}
+	_ = omega
+
+	// os.Args[6] takes dT argument
+	dT, err6 := strconv.ParseFloat(os.Args[6], 64)
+	if err6 != nil {
+		panic(err6)
+	}
+	_ = dT
 }
