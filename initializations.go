2e9e7c2445344b00385b72afcdadd41e6fd8f6b4
Lu-Qiao <luqiao@andrew.cmu.edu>
2022-11-27 20:06:54 -0500

    initialization

--- initializations.go
+++ initializations.go
@@ -1,1 +1,47 @@
 package main
+
+import (
+	"math/rand"
+	"time"
+)
+
+// InitializeTissue
+// Input
+// Output
+func InitializeTissue(width int) Board {
+	var initialTissue Board
+	initialTissue = make([][]Cell, width)
+	for r := range initialTissue {
+		initialTissue[r] = make([]Cell, width)
+	}
+	return initialTissue
+}
+
+// AssignStart
+// input
+// Output
+func AssignStart(initialTissue Board, initialPositions []OrderedPair, conc float64) Board {
+	for _, p := range initialPositions {
+		initialTissue[p.x][p.y].state = "Infectious"
+		initialTissue[p.x][p.y].concVirus = conc
+	}
+	return initialTissue
+}
+
+// RandomStart
+// Input
+// Output
+func RandomStart(initialTissue Board, numPositions int, conc float64) Board {
+	for i := 0; i < numPositions; i++ {
+		time.Sleep(time.Millisecond)
+		rand.Seed(time.Now().UnixNano())
+		row := rand.Intn(len(initialTissue))
+		time.Sleep(time.Millisecond)
+		rand.Seed(time.Now().UnixNano())
+		col := rand.Intn(len(initialTissue))
+
+		initialTissue[row][col].state = "Infectious"
+		initialTissue[row][col].concVirus = conc
+	}
+	return initialTissue
+}
